522e44cefa5fe6ce8621abd184e4780c3b4faddc
Shukrullo Rakhmonov <101639551+shukrulloh01@users.noreply.github.com>
2022-10-05 15:29:48 +0500

    new

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from fastai.vision.all import *
+
+
+
+st.title('Guruhlovchi model\n Bird Insect Animal')
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+    model = load_learner('/Users/shukrullo/Desktop/creature/first_model.pkl')
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
